35fa47689a713d271b350c5f8782ba497dfac9c0
urano <darrenlowell_2226240033@mhs.mdp.ac.id>
2024-11-30 12:33:02 +0700

    finale update

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -14,146 +14,167 @@ class MainApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('2226240033'),
         ),
-        body: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
+        body: const SafeArea(
+          child: TimnasIndonesiaPage(),
+        ),
+      ),
+    );
+  }
+}
+
+class TimnasIndonesiaPage extends StatefulWidget {
+  const TimnasIndonesiaPage({super.key});
+
+  @override
+  _TimnasIndonesiaPageState createState() => _TimnasIndonesiaPageState();
+}
+
+class _TimnasIndonesiaPageState extends State<TimnasIndonesiaPage> {
+  bool isFavorite = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Image.asset(
+            'images/timnas_cover.jpeg',
+            fit: BoxFit.cover,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Image.asset(
-                  'images/timnas_cover.jpeg',
-                  fit: BoxFit.cover,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        'TIMNAS INDONESIA',
-                        style: TextStyle(
-                          fontSize: 24.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.favorite_border),
-                        onPressed: () {
-                         
-                        },
-                      ),
-                    ],
+                const Text(
+                  'TIMNAS INDONESIA',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Row(
-                        children: [
-                          Icon(Icons.person),
-                          SizedBox(width: 8.0),
-                          Text('Pelatih: Shin Tae Yong'),
-                        ],
-                      ),
-                      SizedBox(height: 8.0),
-                      Row(
-                        children: [
-                          Icon(Icons.calendar_today),
-                          SizedBox(width: 8.0),
-                          Text('Didirikan: 1930'),
-                        ],
-                      ),
-                      SizedBox(height: 8.0),
-                      Row(
-                        children: [
-                          Icon(Icons.location_on),
-                          SizedBox(width: 8.0),
-                          Text('Stadion: Gelora Bung Karno'),
-                        ],
-                      ),
-                    ],
+                IconButton(
+                  icon: Icon(
+                    isFavorite ? Icons.favorite : Icons.favorite_border,
+                    color: isFavorite ? Colors.red : Colors.black,
                   ),
-                ),
-                const SizedBox(height: 16.0),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text(
-                        'Tentang',
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 8.0),
-                      Text(
-                        'Tim nasional sepak bola Indonesia adalah tim yang mewakili Indonesia dalam kompetisi internasional sepak bola. Tim ini memiliki sejarah panjang dan telah berpartisipasi dalam berbagai turnamen tingkat dunia dan Asia.',
-                        textAlign: TextAlign.justify,
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        'Pemain',
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(height: 8.0),
-                      GridView.builder(
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 3,
-                          crossAxisSpacing: 8.0,
-                          mainAxisSpacing: 8.0,
-                          childAspectRatio: 0.75,
-                        ),
-                        itemCount: players.length,
-                        itemBuilder: (context, index) {
-                          return Column(
-                            children: [
-                              Container(
-                                width: 80.0,
-                                height: 80.0,
-                                decoration: BoxDecoration(
-                                  color: Colors.grey[300],
-                                  borderRadius: BorderRadius.circular(8.0),
-                                ),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(8.0),
-                                  child: Image.network(
-                                    playerImages[index],
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                              ),
-                              const SizedBox(height: 4.0),
-                              Text(
-                                players[index],
-                                textAlign: TextAlign.center,
-                                style: const TextStyle(fontSize: 14.0),
-                              ),
-                            ],
-                          );
-                        },
-                      ),
-                    ],
-                  ),
+                  onPressed: () {
+                    setState(() {
+                      isFavorite = !isFavorite;
+                    });
+                  },
                 ),
               ],
             ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Row(
+                  children: [
+                    Icon(Icons.person),
+                    SizedBox(width: 8.0),
+                    Text('Pelatih: Shin Tae Yong'),
+                  ],
+                ),
+                SizedBox(height: 8.0),
+                Row(
+                  children: [
+                    Icon(Icons.calendar_today),
+                    SizedBox(width: 8.0),
+                    Text('Didirikan: 1930'),
+                  ],
+                ),
+                SizedBox(height: 8.0),
+                Row(
+                  children: [
+                    Icon(Icons.location_on),
+                    SizedBox(width: 8.0),
+                    Text('Stadion: Gelora Bung Karno'),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 16.0),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'Tentang',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  'Tim nasional sepak bola Indonesia adalah tim yang mewakili Indonesia dalam kompetisi internasional sepak bola. Tim ini memiliki sejarah panjang dan telah berpartisipasi dalam berbagai turnamen tingkat dunia dan Asia.',
+                  textAlign: TextAlign.justify,
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Pemain',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 8.0),
+                GridView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 8.0,
+                    mainAxisSpacing: 8.0,
+                    childAspectRatio: 0.75,
+                  ),
+                  itemCount: players.length,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        Container(
+                          width: 80.0,
+                          height: 80.0,
+                          decoration: BoxDecoration(
+                            color: Colors.grey[300],
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(8.0),
+                            child: Image.network(
+                              playerImages[index],
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 4.0),
+                        Text(
+                          players[index],
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(fontSize: 14.0),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
